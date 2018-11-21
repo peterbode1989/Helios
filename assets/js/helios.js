8fc02b0e5553ef299cb10d1cd455929562d5dede
Peter Bode <peterbode1989@gmail.com>
2018-11-21 14:11:45 +0100

    Default setup

--- assets/js/helios.js
+++ assets/js/helios.js
@@ -1,21 +1,53 @@
 /**
  * Helios
+ *
  * Helios is a lightweight Bootstrap gridslider
  *
- * @author     Peter Bode <peter@acfbentveld.nl>
+ * @package     Helios
+ * @version     1.0.0
+ * @author      Peter Bode <peter@acfbentveld.nl>
  */
 
-(function(c, I, B) {
-    c.fn.helios = function(l) {
-        let a = c.extend({
-            step: 4,
-            show: 4,
-            grid: 12,
-        }, l);
-
-
-        return this.each(function(i, e) {
-            console.info(e);
-        })
+;(function(factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
     }
-})(jQuery, this, 0);
+}(function($) {
+    'use strict';
+    var Helios = window.Helios || {};
+
+    Helios = (function() {
+        function Helios(element, settings) {
+            var _ = this;
+
+            _.defaults = {
+                arrows: false,
+                dots: false,
+                step: 1, // the amount of cols to scroll when moving
+                gutter: 15, // the default space between cols
+            };
+            _.options = $.extend({}, _.defaults, settings);
+
+            _.init();
+        }
+        return Helios;
+    }());
+
+    Helios.prototype.init = function() {
+        var _ = this;
+        console.log(_);
+    };
+
+    $.fn.helios = function() {
+        var _ = this;
+        for (let i = 0; i < _.length; i++) {
+            _[i].helios = new Helios(_[i], arguments[0]);
+        }
+        return _;
+    };
+}));
