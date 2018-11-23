1642d37e6a03ce243537fa5b14edcd5a43d382a0
Peter Bode <peterbode1989@gmail.com>
2018-11-23 11:45:28 +0100

    Improved dot navigation

--- assets/js/helios.js
+++ assets/js/helios.js
@@ -26,7 +26,7 @@
             var _ = this;
 
             _.defaults = {
-                step: 2, // the amount of cols to scroll when moving
+                step: 1, // the amount of cols to scroll when moving
                 gutter: 15, // the default space between cols
                 infinite: false,
 
@@ -39,6 +39,7 @@
                 elPrevArrow: '<button class="helios-arrow prev" aria-label="Previous" type="button">Previous</button>',
                 elNextArrow: '<button class="helios-arrow next" aria-label="Next" type="button">Next</button>',
             };
+
             _.options = $.extend({}, _.defaults, settings);
 
             _.$slider = $(element);
@@ -63,14 +64,14 @@
                     _.$slider.dequeue();
                 });
             });
+            _.update();
         });
     }
 
     Helios.prototype.move = function(_, e) {
         var _ = this;
 
-        let dir = (_.options.step * -$(e.target).attr('data-dir'));
-        _.$currentIndex -= dir;
+        _.$currentIndex -= $(e.target).attr('data-dir');
 
         _.queue( _.$currentIndex * _.$childSize );
     }
@@ -91,14 +92,14 @@
             // Apply default classes and events to elements
             _.$prevArrow = $(_.options.elPrevArrow)
                 .addClass('helios-arrow helios-prev')
-                .attr('data-dir', -1)
+                .attr('data-dir', _.options.step * 1)
                 .on('click', function(e) {
                     _.move(_, e);
                 });
 
             _.$nextArrow = $(_.options.elNextArrow)
                 .addClass('helios-arrow helios-next')
-                .attr('data-dir', 1)
+                .attr('data-dir', _.options.step * -1)
                 .on('click', function(e) {
                     _.move(_, e);
                 });
@@ -116,10 +117,17 @@
 
         if(_.options.dots === true) {
             // do something
-            let ul = $('<ul></ul>');
+            let ul = $('<ul class="helios-dots"></ul>');
 
             for(var i = 0; i < _.$dotCount; i++) {
-                let li = _.options.elDots;
+                let li = $(_.options.elDots)
+                    .text(i)
+                    .attr('data-dir', (i * _.options.step))
+                    .on('click', function(e) {
+                        let step = $(this).attr('data-step');
+                        _.$currentIndex = step;
+                        _.queue( step * _.$childSize );
+                    });
 
                 $(ul).append(li);
             }
@@ -128,13 +136,39 @@
         }
     }
 
-    Helios.prototype.deploy = function() {
+    Helios.prototype.update = function() {
+        var _ = this;
+        console.log(_);
+
+        if(_.$currentIndex == 0 && _.options.infinite === false) {
+            _.$prevArrow.attr('disabled', 'disabled');
+        } else {
+            _.$prevArrow.removeAttr('disabled', 'disabled');
+        }
+        if(_.$childrenCount == (_.$currentIndex + _.options.step) && _.options.infinite === false) {
+            _.$nextArrow.attr('disabled', 'disabled');
+        } else {
+            _.$nextArrow.removeAttr('disabled', 'disabled');
+        }
+
+        if(_.options.dots === true) {
+            $('.helios-dots').children('li').each(function(i) {
+                $(this).removeClass('active');
+                if(_.$currentIndex == i) {
+                    $(this).addClass('active');
+                }
+            });
+        }
+    }
+
+    Helios.prototype.render = function() {
         // all code that appends here.
         var _ = this;
 
         _.buildArrows();
         _.buildDots();
 
+        _.update();
     }
 
     Helios.prototype.init = function() {
@@ -152,7 +186,7 @@
              _.resize();
          });
 
-        _.deploy();
+        _.render();
     };
 
     Helios.prototype.resize = function() {
